179a4b4bde4f8233807eec171c0362315d81a29f
Jeremy Jones <minttoothpick@protonmail.com>
2025-09-04 18:28:39 -0400

    Fix netlify script ESM import

--- netlify/scheduled-deploy.js
+++ netlify/scheduled-deploy.js
@@ -1,15 +1,14 @@
-const fetch = require('node-fetch');
-const { schedule } = require('@netlify/functions');
+import fetch from 'node-fetch';
+import { schedule } from '@netlify/functions';
 
 const BUILD_HOOK =
   'https://api.netlify.com/build_hooks/688bd80b74682c3d42032c98';
 
-const handler = schedule('0 0 * * 1', async () => {
+export const handler = schedule('0 0 * * 1', async () => {
   const response = await fetch(BUILD_HOOK, { method: 'POST' });
   console.log('Build hook response:', response.status);
   return {
     statusCode: 200,
+    body: 'Scheduled deploy triggered',
   };
 });
-
-exports.handler = handler;
